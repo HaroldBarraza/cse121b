c90df3986906292a1ed3a14706515d03fcb2f459
HaroldBarraza <132396790+HaroldBarraza@users.noreply.github.com>
2023-10-25 17:22:50 -0500

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,30 +1,18 @@
-const apikey = "d561b25471e94f0eb00c10ec3e1a0825";
-const weatherInfoElement = document.querySelector('#weather-info');
+const apikey = "932ccbb1a2ee79c0e7df7f8aea195d3e";
 
-async function getWeather() {
-    const locationInput = document.querySelector('#location'); // Cambio 'location' a '#location'
-    const location = locationInput.value;
+export async function getWeatherData(location) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apikey}&units=metric`;
 
-    if (!location) {
-        alert('Please enter a location');
-        return;
+    const response = await fetch(apiUrl);
+
+    if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message);
     }
 
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apikey}&units=metric`; // Cambio 'weatherq' a 'q' y 'unit' a 'units'
+    return response.json();
+}
 
-    try {
-        const response = await fetch(apiUrl);
-        const data = await response.json();
-
-        if (response.ok) {
-            displayWeather(data);
-        } else {
-            displayError(data.message);
-        }
-    } catch (error) {
-        console.error('Error fetching weather data:', error);
-        displayError('An error occurred while fetching weather data'); // Cambio 'ocurred' a 'occurred'
-    }
 }
 
 function displayWeather(data) {
